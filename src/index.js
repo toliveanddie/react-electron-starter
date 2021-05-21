66298f4053f1f144eb716899791bc6407bde0d5a
Norm <toliveanddie76@gmail.com>
2021-05-21 14:24:58 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rando = Math.floor(Math.random() * 10) + 1;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Page extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      result: '',
+      count: 0
+    };
+  }
+
+  clickHandler = () => {
+    var a = document.getElementById('guess').value;
+    this.setState({count: this.state.count + 1})
+    if (a == rando) {
+      this.setState({result: a + ' is Correct!'});
+    } else if (a < rando) {
+      this.setState({result: a + ' is too low, try again.'});
+    } else {
+      this.setState({result: a + ' is too high, try again'});
+    }
+  }
+
+  reloadPage = () => {
+    window.location.reload();
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Guessing game. pick a number from 1 - 10</h1>
+        <br></br>
+        <h3>{this.state.result}</h3>
+        <input id='guess' type='number'></input>
+        <br></br>
+        <button id='enter' className='btn btn-primary' onClick={this.clickHandler}>Enter</button>
+        <br></br>
+        <button id='pick'className='btn btn-info' onClick={this.reloadPage}>Guess a new number</button>
+        <br></br>
+        <p>guesses: {this.state.count}</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
